4590674859171a8518f10879439d5641a3c0655f
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-12 21:42:49 -0300

    Add project files.

---
+++ ProgramConversorDolar/ConversorDeMoeda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramConversorDolar
+{
+    class ConversorDeMoeda
+    {
+        public static double IoF = 6;
+        public static double Conversor(double cot, double quant)
+        {
+            double total = quant * cot;
+            return total + (total * IoF / 100);
+           
+        }
+
+
+    }
+}
---
+++ ProgramConversorDolar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramConversorDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a cotação do dolar: ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de dólar a ser comprada: ");
+            double quantia = double.Parse(Console.ReadLine());
+
+            double result = ConversorDeMoeda.Conversor(cotacao, quantia);
+
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2"));
+
+            Console.ReadLine();
+        }
+    }
+}
